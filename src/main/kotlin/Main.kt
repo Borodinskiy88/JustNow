c0485f92a83d0074e0133c25c4605141483416f7
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-07 18:16:33 +0300

    Вариант с if else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,15 @@
 fun main() {
 
-    val secondAgo = 7201
+    val secondAgo = 333333
     val minutes: Int = secondAgo / 60
     val hours: Int = secondAgo / 3600
 
-    println("был(а) в сети " + lastVisit(minutes, secondAgo, hours))
+    println("был(а) в сети " + agoToText(minutes, secondAgo, hours))
 }
 
+fun agoToText(minutes: Int, secondAgo: Int, hours: Int): String {
 
-//fun agoToText(): String {
-//    val secondAgo = 549
-//    val message = "был(а) в сети"
-//    when(secondAgo) {
-//        in 0..60 -> print("$message только что ")
-//        in 61..60 * 60 -> print("$message х минут назад ")
-//        in 60 * 60 + 1..24 * 60 * 60 -> print("$message x часов назад ")
-//        in 24 * 60 * 60 + 1..24 * 60 * 60 * 2 -> print("$message сегодня ")
-//        in 24 * 60 * 60 * 2 + 1..24 * 60 * 60 * 3 -> print("$message вчера ")
-//        else -> print("давно")
-//    }
-//    return String()
-//}
-
-fun lastVisit(minutes: Int, secondAgo: Int, hours: Int): String {
-
-    return when(secondAgo) {
+    return when (secondAgo) {
         in 0..60 -> "только что "
         in 61..60 * 60 -> "$minutes ${minutesAgo(minutes)} назад "
         in 60 * 60 + 1..24 * 60 * 60 -> "$hours ${hoursAgo(hours)} назад "
@@ -35,30 +20,22 @@ fun lastVisit(minutes: Int, secondAgo: Int, hours: Int): String {
 }
 
 fun minutesAgo(minutes: Int): String {
-//todo 1, 11, 21, 31, 41, 51 минута
+// 1, 11, 21, 31, 41, 51 минута
 // 2-4, 22-24, 32-34, 42-44, 52-54 минуты
 // остальное минут
     return if (minutes % 10 == 1 && minutes != 11) {
         "минуту"
-    } else if (minutes % 10 == 2 || minutes % 10 ==3 || minutes % 10 ==4) {
+    } else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
         "минуты"
     } else {
         "минут"
     }
-////    val minutes: Int = secondAgo / 60
-//    return when(minutes) {
-// //       in 60 * 60..60 * 60 * 2 -1 -> "1 час назад"
-//        (minutes % 10 == 1) -> "минута"
-//        (minutes % 10 == 2 || minutes % 10 ==3 || minutes % 10 ==4) {"минуты"}
-//        else {"минут"}
-//    }
 }
 
 fun hoursAgo(hours: Int): String {
-//todo 1, 21 час
+// 1, 21 час
 // 2, 3, 4, 22, 23 часа
 // 5-20 часов
-// 1 час = 3600 секунд
     return if (hours % 10 == 1) {
         "час"
     } else if (hours % 10 == 2 || hours % 10 == 3 || hours % 10 == 4) {
